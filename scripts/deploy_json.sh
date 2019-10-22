0930989d651d6e6b993e31fb3541ff75d8b1b72e
Andy Lake <andy@es.net>
2019-10-22 09:42:26 -0400

    Removing meshconfig stuff from deploy_json

--- scripts/deploy_json.sh
+++ scripts/deploy_json.sh
@@ -21,22 +21,6 @@ fi
 #if here, then time to update
 git pull
 
-#get list of files
-for f in *.conf
-do
-    MESH=`echo "$f" | sed 's/\(.\)\.conf/\1/'`
-    echo "Building ${MESH}.json"
-    /usr/lib/perfsonar/bin/build_json --input $f --output /tmp/${MESH}.json
-    if [ $? != 0 ]; then
-       echo "Error generating JSON file ${MESH}.json"
-    else
-       cp -f /tmp/${MESH}.json /var/www/html/${MESH}.json
-       rm -f /tmp/${MESH}.json
-       echo "New JSON file available ${MESH}.json"
-     fi
-     echo ""
-done
-
 #copy psconfig files
 cd ../psconfig
 mkdir -p /var/www/html/psconfig
